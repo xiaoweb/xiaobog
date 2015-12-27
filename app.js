615ada0d1f3873afded0957f6ee03ee0a244ecf7
123 <123>
2015-12-27 23:28:28 +0800

    基本结构

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-/**
- * Created by zhou on 2015/11/27.
- */
+/** * Created with WebStorm. * User: RD-小小WEB * Date: 2015/12/26 * Time: ‏‎22:19 */
 var koa = require('koa'),
-    app = koa();
+    router = require('koa-router')(),
+    app = koa(),
+    jade = require('jade'),
+    routes = require('./routes'),
+    fs = require('fs'),
+    mongoose = require('mongoose');
 
-app.use(function*(next) {
-    this.body = "hello world";
+//环境 NODE ENV || development || production
+app.env = 'production';
+
+
+var test  ;
+//test
+app.use(function*(next){
+    yield next.next();
+})
+
+//日志
+app.use(function *(next) {
+    var time = new Date();
     yield next;
-    console.log(this.url)
+    var log = time.toLocaleString() + " " + this.ip + " " + this.method + " " + this.host + this.url + " " + this.status + " " + (new Date().getTime() - time.getTime()).toString() + 'ms';
+    fs.appendFile(__dirname + '/log/log.log', log + '\n', function (err) {
+        err ? console.error(err) : app.env == "development" && console.info(log);
+    })
 });
 
-app.use(function*() {
-    this.body = '123'
+//模板引擎
+app.use(function*(next) {
+    !this.res.render && (this.res.render = function (template, data) {
+        return jade.compileFile(__dirname + '/views/' + template + '.jade', {pretty: true})(data)
+    })
+    yield next;
 })
 
-app.use(function*() {
+//路由
+routes(router);
+app.use(router.routes())
 
-})
-app.listen(3000);
+mongoose.connect('mongodb://xiaowebblog:123456@192.168.1.103:27017/xiaoweb',function(err){
+    if(err){
+        console.log(err);
+    } else{
+        console.log("连接成功")
+    }
+});
+
+//端口
+app.listen(80);
+
+
+
+
+
---
+++ routes.js
@@ -0,0 +1,18 @@
+/** * Created with WebStorm. * User: RD-小小WEB * Date: 2015/12/26 * Time: 0:30 */
+var index = require('./routes/index'),
+    user = require('./routes/user'),
+    login = require('./routes/login')
+
+function routes(router) {
+    //首页
+    router.get("/", index);
+
+    //用户
+    router.get("/user", user);
+
+    //登录页
+    router.get('/login', login)
+}
+
+module.exports = routes;
+
